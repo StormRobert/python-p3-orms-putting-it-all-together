4ac4dc633404bf67357da678f89323b58f0da274
StormRobert <stormarasa2@gmail.com>
2023-12-13 01:02:15 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -41,3 +41,52 @@ class Dog:
         CURSOR.execute(sql,(self.name, self.breed))
         CONN.commit()
         self.id = CURSOR.lastrowid
+    
+    @classmethod
+    def create(cls, name, breed):
+       dog = cls(name, breed)
+       dog.save()
+       return dog
+    @classmethod
+    def new_from_db(cls, row):
+       if row is not None:
+        dog = cls(name = row[1], breed = row[2])
+        dog.id = row[0]
+        return dog
+       else:
+          return cls(None, None)
+    
+    @classmethod
+    def get_all(cls):
+       sql = """
+        SELECT FROM * dogs
+        """
+       all = CURSOR.execute(sql).fetchall()
+       cls.all = [cls.new_from_db(row) for row in all]
+       return cls.all
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT * FROM dogs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        dog = CURSOR.execute(sql,(name,)).fetchone()
+        return cls.new_from_db(dog)
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * FROM dogs
+            WHERE id = ?
+            LIMIT 1
+        """
+
+        dog = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.new_from_db(dog)
+
+        
+
+    
